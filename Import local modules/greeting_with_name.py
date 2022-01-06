bacfb40b773f2083e7b80643d37e226e79eb5bd9
BethDataHub <bethanymarshall97@hotmail.com>
2022-01-06 21:18:06 +0000

    rand num to rand choice

--- Import local modules/greeting_with_name.py
+++ Import local modules/greeting_with_name.py
@@ -4,9 +4,7 @@ from greeting import intro,outro
 
 def get_your_name():
     names = ['Wiona','Hercules','Andor','Rogue','Gibson','Audrey','Waggs','Ollo','Tallis','Magic']
-    num = random.randrange(0,10)
-    yourName = names[num]
-
+    yourName = random.choice(names)
     return yourName
 
 yourName = get_your_name()
